4685be4d242d0252a7e8a4dec24041e78d4bdef1
Jan Meyer <git@lolmerkat.cc>
2025-10-20 02:31:50 +0200

    test: update temp main test

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main;
 
 import (
 	"fmt"
-	"github.com/lolmerkat/delta/internal"
+	"log"
+	cfg "github.com/lolmerkat/delta/internal/config"
 )
 
 func main() {
-	fmt.Printf("Config Dir: %s\n", delta.ConfigDir())
-	fmt.Printf("Config Path: %s\n\n", delta.ConfigPath())
-	config, _ := delta.Default()
-	config.Repositories = append(config.Repositories, delta.Repository{
+	fmt.Printf("Config Dir: %s\n", cfg.ConfigDir())
+	fmt.Printf("Config Path: %s\n\n", cfg.ConfigPath())
+
+	config, _ := cfg.Load()
+	config.Repositories = append(config.Repositories, cfg.Repository {
 		Name: "windows-config",
 		Remote: "https://github.com/lolmerkat/windots",
 		LocalPath: "C:\\Users\\meyer\\.dotfiles\\windows-config\\.config\\",
 	})
+
 	tomlString, _ := config.TomlString()
+	err := config.Write()
+	if err != nil {
+		log.Fatalf("Error writing config: %v", err)
+	}
 	fmt.Printf("Toml String: \n%s\n", tomlString)
 }
